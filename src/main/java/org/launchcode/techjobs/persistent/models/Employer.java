17a5a7b56525938098d857ba358f9af9e88f2ce7
Matthew Temple <mattdtemple@gmail.com>
2022-11-15 21:46:20 -0600

    Added controllers, errors with model attributes

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min = 2, max = 70, message = "Must be between 2 and 70 characters")
+    private String location;
+
+    public Employer () {};
 
 
 }
